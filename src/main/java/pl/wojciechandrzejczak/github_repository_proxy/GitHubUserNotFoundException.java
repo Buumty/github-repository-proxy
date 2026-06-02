53842fb20be968fa75d120fc8068a643d7b636ed
Buumty <wojtekand22@gmail.com>
2026-06-02 18:22:03 +0200

    Refactor of Exception Class

--- src/main/java/pl/wojciechandrzejczak/github_repository_proxy/GitHubUserNotFoundException.java
+++ src/main/java/pl/wojciechandrzejczak/github_repository_proxy/GitHubUserNotFoundException.java
@@ -1,6 +1,6 @@
 package pl.wojciechandrzejczak.github_repository_proxy;
 
-public class GitHubUserNotFoundException extends RuntimeException{
+class GitHubUserNotFoundException extends RuntimeException{
     GitHubUserNotFoundException(String username) {
         super("Github user not found: " + username);
     }
